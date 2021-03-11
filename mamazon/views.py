eb4be9aa07a8e8af5fba053fd8dad303ed2704d5
kusama <dayecaojian@gmail.com>
2021-03-11 17:49:23 +0900

    lesson5

--- mamazon/views.py
+++ mamazon/views.py
@@ -1,5 +1,17 @@
-from django.views.generic import TemplateView
-
+from django.views.generic import TemplateView, ListView
+from .models import Product
 
 class Home(TemplateView):
     template_name = 'mamazon/home.html'
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'mamazon/list.html'
+
+    #検索窓の実装
+    def get_queryset(self):
+        queryset = Product.objects.all()#全ての商品情報を取得
+        if 'query' in self.request.GET:#検索されたとき
+            qs = self.request.GET['query']#検索された情報を格納
+            queryset = queryset.filter(name__contains=qs)
+        return queryset
