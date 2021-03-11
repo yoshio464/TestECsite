be3e04fc325358a9abc955bb3b6698e104ab6928
kusama <dayecaojian@gmail.com>
2021-03-11 17:04:45 +0900

    lessons4

--- mamazon/models.py
+++ mamazon/models.py
@@ -6,3 +6,7 @@ class Product(models.Model):
     price = models.IntegerField()
     description = models.TextField()
     thumbnail = models.ImageField(upload_to='thumbnails/')
+
+    def __str__(self):
+        return self.name
+    
